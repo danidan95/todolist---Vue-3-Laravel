18ebf801a5d0bd907aa72cfef1035c664bd0e0cd
Dani Dan <dani.dan_95@yahoo.ro>
2023-02-14 00:29:31 +0200

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +15,13 @@ use App\Http\Controllers\ItemController
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group( function ( ) {
+        Route::post('/store', [ItemController::class, 'store']);
+        Route::put('/{id}', [ItemController::class, 'update']);
+        Route::delete('/{id}', [ItemController::class, 'destroy']);
 });
