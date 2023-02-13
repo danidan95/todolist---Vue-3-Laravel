75301dbee4468a4589fe3de0d44d9d0e0cb84365
Dani Dan <dani.dan_95@yahoo.ro>
2023-02-14 01:19:07 +0200

    initialize frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import App from './vue/app.vue'
+
+const app = createApp({})
+
+app.component('welcome', App);
+
+app.mount('#app')
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <h1> dnavsisdovna </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+        title: 'How To Install Vue 3 in Laravel 9 From Scratch'
+    })
+}
+</script>
